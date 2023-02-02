3175528de963fdb53a7574af2c6976ce75ebed65
DBA <7777_7788@mail.ru>
2023-02-02 18:54:44 +0300

    Updata model User

--- api/models.py
+++ api/models.py
@@ -12,33 +12,11 @@ class User(AbstractUser):
     user_role = models.CharField(max_length=10, choices=USER_ROLE, default='USER') 
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['email', 'password']
+    REQUIRED_FIELDS = ['email']
      
         
     def __str__(self):
-        return self.email      
-        
-        
-    def get_full_name(self):
-        '''
-        Возвращает first_name и last_name с пробелом между ними.
-        '''
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
-    
-
-    def get_short_name(self):
-        '''
-        Возвращает сокращенное имя пользователя.
-        '''
-        return self.first_name    
-     
-        
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        '''
-        Отправляет электронное письмо этому пользователю.
-        '''
-        send_mail(subject, message, from_email, [self.email], **kwargs)
-        
+        return self.email            
+          
         
      
