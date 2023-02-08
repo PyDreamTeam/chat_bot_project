6eb0643db25ebf5d7405732161f0cbce9617c4a4
Karina Predybailo <karinadurko5@gmail.com>
2023-02-08 23:29:27 +0300

    Add token in response

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+from djoser.conf import settings
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -5,29 +7,21 @@ from .models import User
 from .serializers import UserCreateSerializer
 from djoser import views
 
-# class UserRegistration(APIView):
-#
-#     def post(self, request):
-#         serializer = UserCreateSerializer(data=request.data)
-#
-#         if not serializer.is_valid():
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#         User.objects.create_user(
-#             serializer.data.get('email'),
-#             serializer.data.get('password')
-#         )
-#         return Response(status=status.HTTP_201_CREATED)
-
 
 class UserViewSet(views.UserViewSet):
 
+    @transaction.atomic
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
+        user = User.objects.get(email=request.data.get('email'))
         headers = self.get_success_headers(serializer.data)
-        return Response(status=status.HTTP_201_CREATED, headers=headers)
+        token, _ = settings.TOKEN_MODEL.objects.get_or_create(user=user)
+        token_serializer_class = settings.SERIALIZERS.token
+        return Response(
+            data=token_serializer_class(token).data,
+            status=status.HTTP_201_CREATED, headers=headers)
 
 
 class UserApiView(APIView):
