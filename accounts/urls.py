910299360610b965f59159e592016e59d72172ed
Valery Tsischanka <houllet95@gmail.com>
2023-02-19 16:00:16 +0300

    Add settings for reset_password_confirm, add path for reset_password_confirm in url, add .env.example

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
+from accounts.views import UserViewSet
+
+
 
 from . import views as views_accounts
 
@@ -11,5 +14,6 @@ urlpatterns = [
     path('userlist/<int:pk>/', views_accounts.UserApiView.as_view()),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),  # djoser
+    re_path(r'^user/reset_password_confirm/(?P<uid>[\w-]+)/(?P<token>[\w-]+)/$', UserViewSet.as_view({'post': 'reset_password_confirm'}, template_name='registration/password_reset_confirm.html', name='password_reset_confirm'))
 ] + router.urls
 
