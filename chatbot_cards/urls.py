9ff1556f6ac2b3c3b6cff138144c373a52306150
Valery Tsischanka <houllet95@gmail.com>
2023-03-12 01:09:17 +0300

    Add serializars, vievs, urls for chatbot_cards

---
+++ chatbot_cards/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import *
+
+router = routers.DefaultRouter()
+router.register(r'bots', BotsViewSet)
+router.register(r'business-area', BusinessAreaViewSet)
+router.register(r'business-target', BusinessTargetViewSet)
+router.register(r'functional', FunctionalViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
