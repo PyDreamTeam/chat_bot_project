9284bf5a7e533557499176bef3adab508dbc4bf1
Сергей Готовка <112329009+SerzGotovka@users.noreply.github.com>
2023-03-20 09:15:51 +0300

    Update models.py

--- chatbot_favourites/models.py
+++ chatbot_favourites/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from accounts.models import User
+from chatbot_cards.models import Bots
 
 from chat_bot_project.chatbot_cards.models import Bots
 
 class Bot_favourites(models.Model):
     bot_favourites = models.ManyToManyField(Bots, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+
+    def display_products(self):
+        return ', '.join([bots.title for bots in self.bot_favourites.all()])
 
