7b4af206e896a2cf41349d765a5c4c7fa5ffbb1f
DBA <7777_7788@mail.ru>
2023-03-26 20:05:30 +0300

    added Dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "wait"
+
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+      sleep 0.1
+    done
+
+    echo "run"
+fi
+
+python manage.py migrate
+
+exec "$@"
