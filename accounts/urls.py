23247fedb2951e698bd92bb97a8e7c3dff7a6c73
Mazhar Uladzislau <39065854+DoctorBA@users.noreply.github.com>
2023-05-10 15:36:34 +0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from . import views as views_accounts
-from .views import CustomTokenCreateView, CustomTokenDestroyView
 
 
 router = DefaultRouter()
@@ -13,7 +12,7 @@ urlpatterns = [
     path('change-password/', views_accounts.ChangePasswordView.as_view(), name='change-password'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),  # djoser
-    path('auth/token/create/', CustomTokenCreateView.as_view(), name='token_create'),
-    path('auth/token/destroy/', CustomTokenDestroyView.as_view(), name='token_destroy'),
+    path('auth/token/create/', views_accounts.CustomTokenCreateView.as_view(), name='token_create'),
+    path('auth/token/destroy/', views_accounts.CustomTokenDestroyView.as_view(), name='token_destroy'),
     #re_path(r'^user/reset_password_confirm/(?P<uid>[\w-]+)/(?P<token>[\w-]+)/$', views_accounts.UserViewSet.as_view({'post': 'reset_password_confirm'}, name='password_reset_confirm'))
 ] + router.urls
