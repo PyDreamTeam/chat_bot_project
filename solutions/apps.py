c190c896379ffecfa4eb9850b11dd387297adfd1
Сергей Готовка <112329009+SerzGotovka@users.noreply.github.com>
2023-07-10 14:50:33 +0300

    create app Solutions

---
+++ solutions/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SolutionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'solutions'
---
+++ solutions/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+# class Solutions(models.Model):
+#     platform = models.ForeignKey(Platforms, on_delete=models.CASCADE)
+    
