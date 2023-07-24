1c30265cdd8c202eba3c2599aa10ac7513854622
Anatol Charnou <chernov_anatoliy@tut.by>
2023-07-24 20:06:51 +0300

    fix for pull request

--- config/settings.py
+++ config/settings.py
@@ -16,8 +16,6 @@ from datetime import timedelta
 from typing import Dict, Any
 
 import environ
-import dotenv
-dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
