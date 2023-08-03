f380b0bf143ac2a94b0007d6314ed625fa9a027f
Ирина <1r04k@mail.ru>
2023-08-03 21:10:19 +0300

    created new migrations

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+# from .models import Order
+#
+#
+# @admin.register(Order)
+# class OrderAdmin(admin.ModelAdmin):
+#     list_display = ('first_name', 'email', 'phone_number', 'comment')
