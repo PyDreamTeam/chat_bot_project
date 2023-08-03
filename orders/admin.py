9530f6061d24ed531a2980eda19659706f56f8f2
Valery Tsischanka <houllet95@gmail.com>
2023-08-03 20:13:25 +0300

    Update OrderAPICreate and add Orders to DjangoAdministration

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from orders.models import Order
+
+
+# Register your models here.
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "phone", "text",)
+
+
+from django.contrib import admin
+
 # Register your models here.
--- orders/views.py
+++ orders/views.py
@@ -4,10 +4,32 @@ from rest_framework.response import Response
 from .models import Order
 from .serializers import OrderSerializer
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-class OrderAPICreate(generics.CreateAPIView):
-    queryset = Order.objects.all()
+class OrderAPICreate(APIView):
+    permission_classes = [AllowAny]
     serializer_class = OrderSerializer
+
+    def post(self, request):
+        if request.user.is_authenticated:
+            user = request.user
+            full_name = f"{user.first_name} {user.last_name}"
+            existing_data = {
+                'username': full_name,
+                'email': user.email,
+            }
+            data = {**existing_data, **request.data}
+        else:
+            data = request.data
+
+        serializer = OrderSerializer(data=data)
+        if serializer.is_valid():
+            application = serializer.save()
+            return Response({'message': "Successfully created", 'id': application.id}, status=201)
+        else:
+            return Response(serializer.errors, status=400)
 
 
 class OrderAPIList(generics.ListAPIView):
