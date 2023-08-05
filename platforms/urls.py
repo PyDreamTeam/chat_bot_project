53e6c91b8719e5699d55bda7fbbd632ac47def0b
Anatol <chernov.py.dev@gmail.com>
2023-08-05 12:16:40 +0300

    fix filtration in platform

--- platforms/urls.py
+++ platforms/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (PlatformFilterViewSet, PlatformFiltration,
-                    PlatformGroupViewSet, PlatformSearch, PlatformTagViewSet,
+                    PlatformGroupViewSet, PlatformTagViewSet,
                     PlatformViewSet)
 
 router = DefaultRouter()
@@ -15,6 +15,6 @@ urlpatterns = [
     path(
         "platform/filtration/", PlatformFiltration.as_view(), name="platform-filtration"
     ),
-    path("platform/search/", PlatformSearch.as_view(), name="platform-search"),
+    # path("platform/search/", PlatformSearch.as_view(), name="platform-search"),
     path("platform/", include(router.urls)),
 ]
