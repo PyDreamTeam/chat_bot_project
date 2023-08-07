bdc26d56e7e0301fe2494ac596ea940c78d237f7
Ирина <1r04k@mail.ru>
2023-08-07 22:26:09 +0300

    updated code in serializers.py

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,11 +1,28 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import Order
+
+User = get_user_model()
 
 
 class OrderSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(source='user.first_name')
-    email = serializers.CharField(source='user.email')
+    email = serializers.EmailField(source='user.email')
+    phone_number = serializers.CharField()
+    comment = serializers.CharField()
 
     class Meta:
         model = Order
         fields = ['first_name', 'email', 'phone_number', 'comment']
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user', {})
+        user_first_name = user_data.get('first_name')
+        user_email = user_data.get('email')
+
+        user = User.objects.create(first_name=user_first_name, email=user_email)
+
+        order = Order.objects.create(user=user,
+                                     phone_number=validated_data['phone_number'],
+                                     comment=validated_data['comment'])
+        return order
