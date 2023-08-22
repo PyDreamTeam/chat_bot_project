aeca450b5a34e79f5ce191c88b3d8b82998bf9bb
DBA <7777_7788@mail.ru>
2023-08-22 13:40:47 +0300

    connection app solutions

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,8 @@
 from orders.models import Order
 from django.contrib import admin
 
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ("username", "email", "phone", "text",)
+# @admin.register(Order)
+# class OrderAdmin(admin.ModelAdmin):
+#     list_display = ("username", "email", "phone", "text",)
 
 
