66fbec1eac0d3a41b65d058a36f7bd453a4c8afe
Ирина <1r04k@mail.ru>
2023-08-28 17:02:09 +0300

    views updated

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,7 @@ from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, Bl
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 from drf_spectacular.utils import extend_schema
@@ -43,4 +44,11 @@ class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get_object(self):
+        user = self.request.user
+        profile = Profile.objects.get(user=user)
+
+        return profile
 
