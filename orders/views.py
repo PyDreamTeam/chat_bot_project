624c05357f486b9a7ee5dc1649026a46a3daeb6c
Ирина <1r04k@mail.ru>
2023-09-03 11:30:22 +0300

    added a method for correctly filling in the fields in the database, URLs for deleting and getting records by id

--- orders/views.py
+++ orders/views.py
@@ -1,36 +1,40 @@
 from rest_framework.permissions import AllowAny
-from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet
 from .models import Order
 from .serializers import OrderSerializer
 from django.utils import timezone
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
-"""
-OrdersViewSet processes applications from registered and unregistered users
-and save them in the database
-"""
 
-class OrdersViewSet(mixins.CreateModelMixin,
-                   mixins.RetrieveModelMixin,
-                   mixins.UpdateModelMixin,
-                   mixins.DestroyModelMixin,
-                   mixins.ListModelMixin,
-                   GenericViewSet):
-
+# OrdersViewSet processes applications from registered and unregistered users and save them in the database
+class OrdersViewSet(viewsets.GenericViewSet,
+                    viewsets.mixins.CreateModelMixin,
+                    viewsets.mixins.RetrieveModelMixin,
+                    viewsets.mixins.UpdateModelMixin,
+                    viewsets.mixins.DestroyModelMixin,
+                    viewsets.mixins.ListModelMixin):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [AllowAny]
+    authentication_classes = [TokenAuthentication]
+
+    # этот метод для получения заявок по токену (пока не работает, как нужно)
+    def get_queryset(self):
+
+        if self.request.user.is_authenticated:
+
+            return Order.objects.filter(user=self.request.user)
+
+        else:
+            return Order.objects.all()
 
     def perform_create(self, serializer):
-        """
-        registered users are processed here and their data is stored in the database.
-        """
+
+        # registered users are processed here and their data is stored in the database.
         if self.request.user.is_authenticated:
 
             serializer.save(user=self.request.user, created_time=timezone.now())
-            """
-            unregistered users are processed here and their data is stored in the database.
-            """
+
+        # unregistered users are processed here and their data is stored in the database.
         else:
             serializer.save(created_time=timezone.now())
-
