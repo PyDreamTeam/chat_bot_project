ed33a8e7308d7c2860e0e5d9892508213c596398
zhdaniukivan <iz2819815@gmail.com>
2023-09-22 14:47:12 +0300

    fixed status code

--- favorite/favorite.py
+++ favorite/favorite.py
@@ -3,6 +3,9 @@ from favorite.models import Favorite
 from requests import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
+from django.db.models import Exists, OuterRef
 
 
 class ManageFavorite:
@@ -30,3 +33,13 @@ class ManageFavorite:
                 {'message': 'Контент удален из избранного'},
                 status=status.HTTP_200_OK
             )
+
+    def annotate_qs_is_favorite_fields(self, queryset):
+        if self.request.user.is_authenticated:
+            is_favorite_subquery = Favorite.objects.filter(
+                object_id=OuterRef('pk'),
+                user=self.request.user,
+                content_type=ContentType.objects.get_for_model(queryset.model)
+            )
+            queryset =queryset.annotate(is_favorite=Exists(is_favorite_subquery))
+        return queryset
